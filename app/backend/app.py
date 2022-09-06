9de714c1697c2ba1c1f74b6d6b4b34e42fa0e58c
Gabriel Oliveira <gabrielfrox@gmail.com>
2022-09-06 11:12:38 -0300

    All crud routes done and some refactoring on database.py

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request
 import database
 
 app = Flask(__name__)
 
-@app.route("/")
-def landing_page():
-  return database.get_beneficiaries()
+
+@app.route("/", methods=['GET'])
+def landing_page_content():
+    return database.get_beneficiaries()
+
+
+@app.route("/create", methods=["POST"])
+def create_new_beneficiary():
+    payload = request.get_json()
+    result = database.create_beneficiary(payload)
+    return result
+
+
+@app.route('/update', methods=["PUT"])
+def update_beneficiary():
+  payload = request.get_json()
+  result = database.update_beneficiary(payload)
+  return result
+
+
+@app.route('/delete', methods=["DELETE"])
+def delete_beneficiary():
+  payload = request.args
+  result = database.delete_beneficiary(payload["cpf"])
+  return result
--- app/backend/database.py
+++ app/backend/database.py
@@ -1,26 +1,49 @@
 import json
+import string
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.proagro_facil
 beneficiary_collection = db.beneficiaries
 
+
+# Just a helper
+def check_existence_by_cpf(cpf: string):
+    result = beneficiary_collection.find_one({"cpf": cpf})
+    return True if result else False
+
 # Recover every beneficiary from collection
 def get_beneficiaries():
-  result = beneficiary_collection.find({}, {"_id": False})
-  return list(result)
+    result = beneficiary_collection.find({}, {"_id": False})
+    return list(result)
+
 
 # Create a new document with beneficiary information
-def create_beneficiary(person: json):
-  new_beneficiary = beneficiary_collection.insert_one(person)
-  return new_beneficiary.inserted_id
+def create_beneficiary(person_data: json):
+    check = check_existence_by_cpf(person_data["cpf"])
+    if check:
+        return "CPF já cadastrado!"
+    else:
+        new_beneficiary = beneficiary_collection.insert_one(person_data)
+        return f'Beneficiário cadastrado com sucesso! ID: {new_beneficiary.inserted_id}'
 
-# Update one beneficiary information and check his existence
+
+# Update one beneficiary information after check his existence
 def update_beneficiary(new_data: json):
-  cpf = new_data["cpf"]
-  check_existence = beneficiary_collection.find_one({"cpf": cpf})
-  if check_existence:
-    beneficiary_collection.update_one({"cpf": cpf}, {"$set": new_data})
-    return "Atualizado com sucesso!"
-  else:
-    return "CPF não encontrado!"
+    cpf = new_data["cpf"]
+    check = check_existence_by_cpf(cpf)
+    if check:
+        beneficiary_collection.update_one({"cpf": cpf}, {"$set": new_data})
+        return "Atualizado com sucesso!"
+    else:
+        return "CPF não encontrado!"
+
+
+# Delete beneficiary from collection
+def delete_beneficiary(cpf: string):
+    check = check_existence_by_cpf(cpf)
+    if check:
+        beneficiary_collection.delete_one({"cpf": cpf})
+        return "Beneficiário deletado com sucesso!"
+    else:
+        return "Beneficiário não encontrado"
